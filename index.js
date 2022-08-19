41271d153d33116daa0f0bad46d25c890e921fd7
Gift13-07 <giftchinwendu07@gmail.com>
2022-08-19 21:48:51 +0100

    git commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-let view_more
+// let view_more= document.getElementById("view-more")
+// let menu = document.getElementById("menu")
+// let open = false;
+// view_more.addEventListener("click", () => {
+//     if(open === false) {
+//         open = true
+//         menu.style.right = 10
+//     } else {
+//         open = false
+//         menu.style.right = -100
+//     }
+// })
