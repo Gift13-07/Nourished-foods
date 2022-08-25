3d1ebd9f88cc570910568f89154cb7b7ae1945da
Gift13-07 <giftchinwendu07@gmail.com>
2022-08-25 13:51:23 +0100

    git commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let view_more= document.getElementById("top")
 let menu = document.getElementById("menu")
 let open = false;
 view_more.addEventListener("click", () => {
-    console.log("jjj")
     if(open === false) {
         open = true
         menu.style.right ="1px";
